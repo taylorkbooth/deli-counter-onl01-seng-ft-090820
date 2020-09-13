5f33f8283a1d1a58293f3e371cc6f9b8d82672cd
Taylor Booth <taylorkbooth@gmail.com>
2020-09-13 01:33:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,12 @@ another_deli = []
 
 
 
-def line(katz_deli,other_deli)
-  if katz_deli == []
+def line(katz_deli)
+  if katz_deli.size == 0
     puts "The line is currently empty."
-  end
-end
+  else
+  puts "Currently serving #{katz_deli}."
+  katz_deli.shift
+ end
+
+ end
