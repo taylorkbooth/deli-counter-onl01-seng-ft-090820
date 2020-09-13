af0144d2f22544a1211dfcfd8d1b0a88fe873f55
Taylor Booth <taylorkbooth@gmail.com>
2020-09-13 01:40:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,10 +2,14 @@
 
 
 def line(katz_deli)
-  if katz_deli == 0
-    puts "The line is currently empty."
+    if array.length >= 1
+    nuarray = []
+    counter = 1 
+    array.each do |name|
+      nuarray.push("#{counter}. #{name}")
+      counter += 1 
+    end 
+    puts "The line is currently: #{nuarray.join(" ")}"
   else
-    new_array=[]
-    puts ""
-  end
+    puts "The line is currently empty."
 end
