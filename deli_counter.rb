2df28795c893dc2a10fc0437477a039cccab2461
Taylor Booth <taylorkbooth@gmail.com>
2020-09-13 01:28:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+katz_deli = []
+other_deli = []
+another_deli = []
+
+
+
+def line(katz_deli,other_deli)
+  if katz_deli == []
+    puts "The line is currently empty."
+  end
+end
